8bed6e1e745da0ea424bc4efdca28056bf12b856
Gustavo Damasceno <x327328@prservicos.com.br>
2024-08-12 11:57:35 -0300

    host flask

--- flask-server.py
+++ flask-server.py
@@ -238,5 +238,5 @@ def exchange_certs():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='192.168.15.3', port=8080)
+    app.run(host='0.0.0.0', port=8080)
     # gunicorn -w 4 -b 192.168.15.4:5000 examples.flask-server:app
